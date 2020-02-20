3ec22be9e7e2aedfbec70659f78bb1319e74c10c
Corbin March <marchcorbin@gmail.com>
2020-02-20 15:00:45 -0700

    Populate cards functionality

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var rightSide = document.querySelector('.right-side');
 var winnerPage = document.querySelector('.winner-page')
 var deck = new Deck()
 
+
 rightSide.addEventListener('click', chooseCard);
 winnerPage.addEventListener('click', restartGame)
 
@@ -10,20 +11,21 @@ window.onload = invokeDeck();
 function invokeDeck() {
   deck.createDeck();
   addStyle();
+  addCardsToDOM();
 }
 
 function addStyle(){
  var styles = [
-   '.image-slant1',
-   '.image-slant2',
-   '.image-slant3',
-   '.image-slant4',
-   '.image-slant5',
-   '.image-slant6',
-   '.image-slant7',
-   '.image-slant8',
-   '.image-slant9',
-   '.image-slant10'
+   'image-slant1',
+   'image-slant2',
+   'image-slant3',
+   'image-slant4',
+   'image-slant5',
+   'image-slant6',
+   'image-slant7',
+   'image-slant8',
+   'image-slant9',
+   'image-slant10'
  ];
  for(var i = 0; i < styles.length; i++){
    deck.cards[i].style = styles[i];
@@ -32,6 +34,11 @@ function addStyle(){
 
 function addCardsToDOM(){
 
+for (var i = 0; i < deck.cards.length; i++){
+  rightSide.insertAdjacentHTML('beforeend',
+  `<div class=${deck.cards[i].style}><img id="card" src=${deck.cards[i].matchedInfo}>
+  </div>`)
+}
 }
 
 
